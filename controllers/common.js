2eeabe66de26b39ceceeab9aa6c48f8608e2567a
Somesh <someshsomani457@gmail.com>
2023-04-12 19:19:01 +0530

    getting all the classrooms of clg

--- controllers/common.js
+++ controllers/common.js
@@ -31,7 +31,25 @@ const createNewClg = catchAsyncErrors(async (req, res) => {
     .json({ success: true, message: "clgs created", data: newClg });
 });
 
+const getClassroomsOfClg = catchAsyncErrors(async (req, res) => {
+  console.log(req.query.clg_id);
+  const data = await Clg.findOne({ _id: req.query.clg_id }).select(
+    "name classrooms"
+  );
+
+  if (!data) {
+    return res
+      .status(400)
+      .json({ success: false, message: "No classrooms found" });
+  }
+
+  res
+    .status(201)
+    .json({ success: true, message: "classrooms found", data: data });
+});
+
 module.exports = {
   newRegistration,
   createNewClg,
+  getClassroomsOfClg,
 };
--- routes/common.js
+++ routes/common.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const verifyToken = require("../middlewares/auth");
-const { newRegistration, createNewClg } = require("../controllers/common");
+const {
+  newRegistration,
+  createNewClg,
+  getClassroomsOfClg,
+} = require("../controllers/common");
 
 const router = express.Router();
 
 router.route("/new-registration").get(newRegistration);
 
 router.route("/create-new-clg").post(createNewClg);
+
+router.route("/get-classrooms-clg").get(getClassroomsOfClg);
 
 module.exports = router;
